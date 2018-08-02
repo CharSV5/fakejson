7e73b1f7599126ae2b6aaa2024388dbad437427a
CharSV5 <charlenebastians@hotmail.com>
2018-08-02 17:01:54 +0100

    basic redux set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 import Posts from './components/Posts';
 import PostsForm from './components/PostsForm';
+import { createStore, applyMiddleware } from 'redux'; 
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +21,7 @@ class App extends Component {
         <hr />
         <Posts />
       </div>
+    </Provider>
     );
   }
 }
