862d3c2618d145c79f7bc170bd5386266da79aca
CharSV5 <charlenebastians@hotmail.com>
2018-08-14 17:00:21 +0100

    posts coming from redux

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -3,8 +3,10 @@ import { FETCH_POST, NEW_POST } from './types';
 export const fetchPosts = () => dispatch => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then(posts => dispatch({
-        type: FETCH_POST,
-        payload: posts
-      }));
-}
+      .then(posts =>
+        dispatch({
+          type: FETCH_POST,
+          payload: posts
+        })
+      );
+    }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions'
 
 class Posts extends React.Component {
 
-
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
 
 
 
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -22,4 +26,8 @@ class Posts extends React.Component {
   }
 }
 
-export default Posts;
+const mapStateToProps = state => ({
+  posts: state.posts.items
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
